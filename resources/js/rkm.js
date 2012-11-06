0422fc31c21a09b07d273c8ac4bad32bbc659ae8
Shayne Koestler <shayne.koestler@kineticdata.com>
2012-11-06 16:46:52 -0600

    cleaned up javascript code

--- resources/js/rkm.js
+++ resources/js/rkm.js
@@ -1,77 +1,71 @@
 jQuery(document).ready(function() {
+    // Here we set the focus to the input box that gathers the search terms
     jQuery("form#searchTerms input#mustHave").focus();
+    
+    // On submit of the search terms form we call the RKMQuery jsp which returns
+    // a json result that represents the matching articles.  We use this json
+    // result to generate an html table.  Each row in the table has a clicked
+    // handler bound to it that retrieves the article (defined below).
     jQuery("form#searchTerms").submit(function() {
         jQuery("table#results tbody").empty();
-        jQuery.ajax({
+        BUNDLE.ajax({
             url: BUNDLE.packagePath + "interface/callbacks/RKMQuery.json.jsp",
             data: jQuery(this).serialize(),
             success: function(data) {
                 var results = jQuery.parseJSON(data);
                 for (var i in results) {
                     var row = jQuery("<tr>");
-                    var zebra;
-                    if (i % 2 === 1) {
-                        zebra = "odd";
-                    } else {
-                        zebra = "even";
-                    }
-                    row.addClass(zebra);
+                    row.addClass((i % 2 === 1) ? "odd" : "even");
                     row.data("article-id", results[i]["Article ID"]);
                     row.data("article-type", results[i]["Source"]);
                     row.append("<td>" + results[i]["Article Title"] + "</td>");
-                    row.append("<td>" + results[i]["Summary"] + "</td>");
                     row.append("<td>" + results[i]["Source"] + "</td>");
-                    row.hover(function() {
-                        jQuery(this).addClass("highlight");
-                    }, function() {
-                        jQuery(this).removeClass("highlight");
-                    });
+                    row.append("<td>" + results[i]["Summary"] + "</td>");
                     jQuery("table#results tbody").append(row);
                     row.click(rowClickHandler);
-
-
                     jQuery("table#results").show();
                 }
             }
         });
-        return false; // This will cancel the default form submit action.
+        return false;
     });
 });
 
+// The loaded array stores the table row dom elements that have been clicked and
+// whose articles have already been successfully loaded.  This is used to ensure
+// that the article jsp is only retrieved once.
 var loaded = [];
-
 function rowClickHandler() {
+    // This object maps the source type to the proper jsp to be called.
+    var jspMap = {
+        "Decision Tree": "decisionTree.html.jsp",
+        "How To": "howTo.html.jsp",
+        "Known Error": "knownError.html.jsp",
+        "Problem Solution": "problemSolution.html.jsp",
+        "Reference": "reference.html.jsp"
+    };
+
+    // Retrieve parameters used in the call from data attributes of the clicked
+    // dom element.
     var articleType = jQuery(this).data("article-type");
     var articleId = jQuery(this).data("article-id");
 
+    // If the loaded array contains the clicked dom element already, its article
+    // has already been loaded.  If the article has not been loaded we make an
+    // ajax call to the proper jsp and add a now row to the table just below the
+    // one clicked and the clicked row is added to the loaded object.  Otherwise
+    // we simple call the slide toggle function to hide and show the article.
     if (jQuery.inArray(this, loaded) === -1) {
-        var jspMap = {
-            "Decision Tree": "decisionTree.html.jsp",
-            "How To": "howTo.html.jsp",
-            "Known Error": "knownError.html.jsp",
-            "Problem Solution": "problemSolution.html.jsp",
-            "Reference": "reference.html.jsp"
-        };
-    
         var clickedRow = this;
-        jQuery.ajax({
+        BUNDLE.ajax({
             url: BUNDLE.packagePath + "interface/callbacks/" + jspMap[articleType],
             data: { articleId: articleId },
             success: function(data) {
-                var articleRow = jQuery("<tr></tr>");
-                articleRow.addClass("articleRow");
-                if (jQuery(clickedRow).hasClass("odd"))
-                    articleRow.addClass("odd");
-                if (jQuery(clickedRow).hasClass("even"))
-                    articleRow.addClass("even");
-                var articleCell = jQuery("<td></td>");
-                articleCell.attr("colspan", "4");
-                var articleContainer = jQuery("<div></div>");
-                articleContainer.addClass("articleContainer");
-                articleContainer.html(data);
-                articleCell.append(articleContainer);
-                articleRow.append(articleCell);
-                jQuery(clickedRow).after(articleRow);
+                var articleRow = jQuery('<tr class="articleRow"></tr>');
+                var articleCell = jQuery('<td colspan="3"></td>');
+                var zebra = (jQuery(clickedRow).hasClass("odd")) ? "odd" : "even";
+                var articleContainer = jQuery('<div class="hidden articleContainer ' + zebra +'">' + data + '</div>');
+                jQuery(clickedRow).after(articleRow.append(articleCell.append(articleContainer)));
                 articleContainer.slideDown();
                 loaded.push(clickedRow);
             }
